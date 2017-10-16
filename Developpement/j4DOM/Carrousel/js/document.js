8aeb6046e61e7a616fff951d596f8ab9a3c6f442
Céline Hédin <hedin.sri@gmail.com>
2017-10-16 21:50:24 +0200

    commit3

--- Developpement/j4DOM/Carrousel/js/document.js
+++ Developpement/j4DOM/Carrousel/js/document.js
@@ -1,4 +1,4 @@
-var index = 0;
+/*var index = 0;
 var t = ["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg"];
 var s = ["lorem1","lorem2","lorem3","lorem4","lorem5","lorem6",]
 //masquer/afficher la barre d'outil :
@@ -10,16 +10,13 @@ toolBar.addEventListener("click",onClicktoolBar);
 function onClicktoolBar(){
   for(var i = 0; i< botton.length; i++){
     botton[i].classList.toggle("hide");
-    console.log("click bouton OK")
+    console.log("click bouton OK");
   }
 }
 
 //inserer les images dans img src du HTML
 var img = document.querySelector("img");
-var titre = document.querySelector("h2");
-
 img.src = t[index];
-titre.textContent = s[index];
 
 /*img.addEventListener("click", onClickImg);
 
@@ -43,21 +40,22 @@ function onClickImg(){
 		sliderImage.src = slides[index];
 	}*/
 
-
-
 //faire en sorte que le bouton next change les images
-var img = document.querySelector("img");
+/*var img = document.querySelector("img");
 var next = document.getElementById("slider-next");
+
 
 next.addEventListener("click", nextClickImg);
 
-function nextClickImg() {
-  if (index === 5){
+
+function nextClickImg(){
+  if(index===5){
     index = 0;
     console.log("remise à 0");
-  } else{
+  }
+  else{
     index++;
-    console.log(index);
+    console.log(index)
   }
   img.src = t[index];
   titre.textContent = s[index];
@@ -68,24 +66,25 @@ function nextClickImg() {
 var previous = document.getElementById("slider-previous");
 previous.addEventListener("click", previousClickImg);
 
-function previousClickImg() {
-  if (index === 0) {
-    index = t.length - 1;
+function previousClickImg(){
+  if(index === 0){
+    index = 5;
     console.log("remise à 0");
-  } else {
-    index--; // index = 0
   }
-
-  img.src = t[index]; // t[0]
+  else{
+      index--;
+  }
+  img.src = t[index];
+  titre.textContent = s[index]; // t[0]
   console.log(index);
-  titre.textContent = s[index];
 }
 
 //Afficher le titre sous les images
-
+var titre = document.querySelector("h2");
+titre.textContent = s[index];
 
 //commander le bouton random
-/*
+
 var random = document.getElementById("slider-random");
 random.addEventListener("click", randomClickImg);
 
@@ -99,5 +98,122 @@ function randomClickImg(){
     index--
     console.log("+1")
   }
+}*/
+
+
+//redisposer le code
+//variables globales
+var t;
+var index;
+var s;
+var slides;
+var timer;
+//function generale
+
+function init(){
+  index = 0;
+  timer = null;
+
+  slides = [
+    {image : "images/1.jpg",legende : "lorem1"},
+    {image : "images/2.jpg",legende : "lorem2"},
+    {image : "images/3.jpg",legende : "lorem3"},
+    {image : "images/4.jpg",legende : "lorem4"},
+    {image : "images/5.jpg",legende : "lorem5"},
+    {image : "images/6.jpg",legende : "lorem6"},
+  ]
+
+  console.log(slides[index].image);
+  console.log(slides[index].legende);
+
+  var toolBar = document.querySelector("p");
+  var botton = document.querySelectorAll("button");
+  var next = document.getElementById("slider-next");
+  var previous = document.getElementById("slider-previous");
+  var random = document.getElementById("slider-random");
+  var play = document.getElementById("slider-toggle");
+
+
+  toolBar.addEventListener("click",onClicktoolBar);
+  next.addEventListener("click", nextClickImg);
+  previous.addEventListener("click", previousClickImg);
+  random.addEventListener("click", randomClickImg);
+  play.addEventListener("click", playClickImg);
+  document.addEventListener("keyup",onKey);
+
+  refresh();
+
 }
-*/
+function refresh(){
+  var img = document.querySelector("img");
+  var titre = document.querySelector("h2");
+
+  img.src = slides[index].image;
+  titre.textContent = slides[index].legende;
+}
+//function evenement
+function onClicktoolBar(){
+  for(var i = 0; i< botton.length; i++){
+    botton[i].classList.toggle("hide");
+    console.log("click bouton OK");
+  }
+}
+
+function nextClickImg(){
+  if(index === 5){
+    index = 0;
+    console.log("remise à 0");
+  }
+  else{
+    index++;
+    console.log(index)
+  }
+  refresh();
+}
+
+function previousClickImg(){
+  if(index === 0){
+    index = 5;
+    console.log("remise à 0");
+  }
+  else{
+      index--;
+  }
+  refresh();
+  console.log(index);
+}
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomClickImg(){
+  index = getRandomInteger(0,slides.length - 1);
+  refresh();
+  console.log(index);
+}
+
+function playClickImg(){
+  var icon = document.querySelector("#slider-toggle i");
+  icon.classList.toggle("fa-play");
+  icon.classList.toggle("fa-pause");
+  if(timer==null){
+    console.log("je suis dans le if")
+    timer = window.setInterval(nextClickImg,1000);
+  }
+  else{
+    window.clearInterval(timer);
+    timer = null;
+  }
+}
+
+function onKey(event){
+  if(event.keyCode==39){
+    nextClickImg();
+  }
+  else if(event.keyCode==37){
+    previousClickImg();
+  }
+}
+
+//code
+document.addEventListener("DOMContentLoaded",init);//appel la fonction init lorsque la page HTML a chargé
---
+++ Developpement/j4DOM/carnet d'adresse/index.js
@@ -0,0 +1,120 @@
+/*/
+//var plus = $("#plus");
+
+var liste = new Array();
+$("#plus").on("click",onClickPlus);
+
+function onClickPlus(){
+  //var section = $(".section");
+  $(".form").toggle();
+
+}
+
+$(".enregistrer").on("click",onClick);
+
+function onClick(){
+  /*var t = [
+    $(".civilite").val(),
+    $(".nom").val(),
+    $(".prenom").val(),
+    $(".tel").val(),
+  ];
+
+  var t = {
+    civilité : $(".civilite").val(),
+    nom : $(".nom").val(),
+    prenom : $(".prenom").val(),
+    tel : $(".tel").val(),
+  }
+  //console.log(t.nom);
+  liste.push(t);
+  //console.log(liste);
+  refresh();
+  $(".form").hide();
+  $(".form").trigger('reset');
+};
+
+function refresh(){
+  var e="";
+  for(var i =0; i<liste.length; i++){
+    e = e + "<li>"+"<a href=''>"+liste[i].nom.toUpperCase()+" "+liste[i].prenom+"</a>"+"</li>"
+  }
+  $("#addressBook").html("<ul>" + e + "</ul>");
+}
+
+$("#trash").on("click",onTrash);
+function onTrash(){
+   liste = [];
+   refresh();
+}*/
+
+//remettre en place le code :
+
+//variables globales
+var liste = new Array();
+
+
+//fonctions evenements
+function onClickPlus(){
+  //var section = $(".section");
+  $(".form").toggle();
+  $("#coordonnee").hide();
+  $("#coordonnee").trigger('reset');
+}
+
+function onClick(){
+  /*var t = [
+    $(".civilite").val(),
+    $(".nom").val(),
+    $(".prenom").val(),
+    $(".tel").val(),
+  ];*/
+
+
+  var t = {
+    civilité : $(".civilite").val(),
+    nom : $(".nom").val(),
+    prenom : $(".prenom").val(),
+    tel : $(".tel").val(),
+  }
+  //console.log(t.nom);
+  liste.push(t);
+  //console.log(liste);
+  refresh();
+  $(".form").hide();
+  $(".form").trigger('reset');
+  $("#coordonnee").trigger('reset');
+};
+
+
+function refresh(){
+  var e="";
+  for(var i =0; i<liste.length; i++){
+    //e = e + "<li>"+"<a href='#' data-index=' "+i+"'>"+liste[i].nom.toUpperCase()+" "+liste[i].prenom+"</a>"+"</li>";
+    e = e + '<li><a href="#" data-index="'+i+'">'+liste[i].nom.toUpperCase()+" "+liste[i].prenom+"</a>"+"</li>";
+  }
+  $("#addressBook").html("<ul>" + e + "</ul>");
+}
+
+function onTrash(){
+   liste = [];
+   refresh();
+   $("#coordonnee").hide();
+   $("#coordonnee").trigger('reset');
+}
+
+function onA(){
+  console.log(this.textContent);
+  var d = $(this).data("index");
+  //console.log(d);
+  //console.log(liste);
+  //console.log(liste[d]);
+  // console.log(liste[0].nom);
+  $("#coordonnee").html("<p>"+liste[d].civilité +" "+liste[d].nom.toUpperCase()+" "+liste[d].prenom +"<br> "+liste[d].tel +"</p>");
+}
+
+//autres codes
+$("#plus").on("click",onClickPlus);
+$(".enregistrer").on("click",onClick);
+$("#trash").on("click",onTrash);
+$(document).on("click","#addressBook a",onA);
